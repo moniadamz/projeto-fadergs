9713ab4f8f1720ff504139ca156d2708eb4619e6
Mônica C. Adamski <monica_adamski@hotmail.com>
2020-11-22 15:13:58 -0300

    fix bug

--- src/controllers/boletoCheckoutController.js
+++ src/controllers/boletoCheckoutController.js
@@ -3,9 +3,9 @@ const boleto = require('../services/payment/boleto');
 
 const boletoCheckoutTransaction = async (request, response, next) => {
     try {
-        const { roomNumber, reference, amount, cpf, name } = request.body;
+        const { reservationId, reference, amount, cpf, name } = request.body;
         
-        const payment = await boleto.makePayment(roomNumber, reference, amount, cpf, name);
+        const payment = await boleto.makePayment(reservationId, reference, amount, cpf, name);
 
         return response.status(200).json(payment);
     } catch (error) {
